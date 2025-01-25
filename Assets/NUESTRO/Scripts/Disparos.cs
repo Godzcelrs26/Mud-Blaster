0037e734508e60b79abc959bcc49b4e1bcaa8bd0
mjuaco <167112346+mjuaco@users.noreply.github.com>
2025-01-25 17:23:28 -0400

    DisqueDisparo

--- Assets/NUESTRO/Scripts/Disparos.cs
+++ Assets/NUESTRO/Scripts/Disparos.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerMotor))]
 public class Disparos : MonoBehaviour
 {
-    [SerializeField]
-    private float Burbujas = 10f;
-
-    public PlayerMotor mr;
-    void Awake()
-    {
-        mr =this.GetComponent<PlayerMotor>();
-    }
+    public GameObject proyectilPrefab; 
+    public Transform puntoDisparo;    
+    public float fuerzaDisparo = 20f;
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Disparar();
+        }
     }
-   
+
+    void Disparar()
+    {
+        GameObject proyectil = Instantiate(proyectilPrefab, puntoDisparo.position, puntoDisparo.rotation);
+
+        Rigidbody rb = proyectil.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(puntoDisparo.forward * fuerzaDisparo , ForceMode.Impulse);
+        }
+    }
 }
